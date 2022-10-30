343476c5628ab2083a3c27f1aa05b4e9d8a0a0bb
Oyindamola32 <oyindamolatolulope82@gmail.com>
2022-10-30 15:29:56 +0100

    modify script

--- script.js
+++ script.js
@@ -39,6 +39,9 @@ const {
 } = restaurant;
 console.log(restaurantName, hours, tags);
 
+const { menu = [], starterMenu: starters = [] } = restaurant;
+console.log(menu, starters);
+
 ///////////////////////////////////////
 //Destructuring Objects
 const { name, openingHours, categories } = restaurant;
