aac480583e001796a24c8b4408c239662cd8d1b3
Oyindamola32 <oyindamolatolulope82@gmail.com>
2022-10-30 16:09:47 +0100

    modify script

--- script.js
+++ script.js
@@ -33,7 +33,12 @@ const restaurant = {
   order(starterIndex, mainIndex) {
     return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
   },
-  orderDelivery: function (starterIndex, mainIndex, time, address) {
+  orderDelivery: function (
+    starterIndex = 1,
+    mainIndex = 0,
+    time = '20.00',
+    address
+  ) {
     console.log(`Order received! ${this.starterMenu} 
       [starterIndex] and ${this.mainMenu[mainIndex]}
       will be delivered to} ${address} at ${time}`);
@@ -45,6 +50,11 @@ restaurant.orderDelivery({
   address: 'via del sole, 21',
   mainIndex: 2,
   starterIndex: 2,
+});
+
+restaurant.orderDelivery({
+  address: 'via del sole, 21',
+  starterIndex: 1,
 });
 
 const { name, openingHours, categories } = restaurant;
