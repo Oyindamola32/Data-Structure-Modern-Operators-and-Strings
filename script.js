1383d6eb6083667d98237426a77376289e19f925
Oyindamola32 <oyindamolatolulope82@gmail.com>
2022-10-31 22:46:13 +0100

    modify script.js

--- script.js
+++ script.js
@@ -26,25 +26,22 @@ const restaurant = {
       close: 24,
     },
   },
-
-  console.log(`Order received! ${this.starterMenu} 
-  [starterIndex] and ${this.mainMenu[mainIndex]}
-  will be delivered to} ${address} at ${time}`);
-},
-
+};
 
 ///////////////////////////////////////
 // The Spread Operator (...)
-const arr =[7, 8, 9];
-const badNewArr=[1, 2, arr[0],arr[1],arr[2]]
+const arr = [7, 8, 9];
+const badNewArr = [1, 2, arr[0], arr[1], arr[2]];
 console.log(badNewArr);
 
-const newArr=[1,2, ...arr]
+const newArr = [1, 2, ...arr];
 console.log(newArr);
 
 console.log(...newArr);
-console.log(1,2,7,8,9);
+console.log(1, 2, 7, 8, 9);
 
+const newMenu = [...restaurant.mainMenu, 'Gnocci'];
+console.log(newMenu);
 /*
   ///////////////////////////////////////
   //Destructuring Objects
