c98a8f8adfb682f61a3568bfd1484c02afff860a
Oyindamola32 <oyindamolatolulope82@gmail.com>
2022-11-02 08:47:10 +0100

    modify script.js

--- script.js
+++ script.js
@@ -50,6 +50,18 @@ const restaurant = {
 };
 
 ///////////////////////////////////////
+// Rest Pattern and Parameters
+// 1) Destructuring
+
+// SPREAD, because on RIGHT side of =
+const arr = [1, 2, ...[3, 4]];
+
+// REST, because on LEFT side of =
+const [a, b, ...others] = [1, 2, 3, 4, 5];
+console.log(a, b, others);
+
+/*
+///////////////////////////////////////
 // The Spread Operator (...)
 const arr = [7, 8, 9];
 const badNewArr = [1, 2, arr[0], arr[1], arr[2]];
@@ -96,7 +108,7 @@ const restaurantCopy = { ...restaurant };
 restaurantCopy.name = 'Ristorante Roma';
 console.log(restaurantCopy.name);
 console.log(restaurant.name);
-/*
+
   ///////////////////////////////////////
   //Destructuring Objects
 
