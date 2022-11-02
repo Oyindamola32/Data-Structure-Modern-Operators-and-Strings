fcf42aaeaf794e8788f02d6aa8edd3979ff7f2d8
Oyindamola32 <oyindamolatolulope82@gmail.com>
2022-11-02 09:20:01 +0100

    modify script.js

--- script.js
+++ script.js
@@ -71,7 +71,9 @@ console.log(pizza, risotto, otherFood);
 
 // 2) Functions
 const add = function (...numbers) {
-  console.log(numbers);
+  let sum = 0;
+  for (let i = 0; i < numbers.length; i++) sum += numbers[i];
+  console.log(sum);
 };
 
 add(2, 3);
