bc50cad6cf35a13b1545acc554d08eb2910b8c27
Oyindamola32 <oyindamolatolulope82@gmail.com>
2022-11-03 13:28:04 +0100

    modify script.js

--- script.js
+++ script.js
@@ -58,7 +58,8 @@ const restaurant = {
 // Logical Assignment Operators
 const rest1 = {
   name: 'Capri',
-  numGuests: 20,
+  //numGuests: 20,
+  numGuests: 0,
 };
 
 const rest2 = {
@@ -66,8 +67,11 @@ const rest2 = {
   owner: 'Giovanni Rossi',
 };
 
-rest1.numGuests = rest1.numGuests || 10;
-rest2.numGuests = rest2.numGuests || 10;
+// OR assignment operator
+// rest1.numGuests = rest1.numGuests || 10;
+// rest2.numGuests = rest2.numGuests || 10;
+// rest1.numGuests ||= 10;  //same as prev
+// rest2.numGuests ||= 10;
 
 console.log(rest1);
 console.log(rest2);
