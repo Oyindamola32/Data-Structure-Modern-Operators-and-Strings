c28e734a13c9b2490f24dac6dfd29722d9011819
Oyindamola32 <oyindamolatolulope82@gmail.com>
2022-11-03 12:35:56 +0100

    modify script.js

--- script.js
+++ script.js
@@ -57,7 +57,7 @@ const restaurant = {
 ///////////////////////////////////////
 // Short Circuiting (&& and ||)
 
-//console.log('---- OR ----');
+console.log('---- OR ----');
 // Use ANY data type, return ANY data type, short-circuiting
 console.log(3 || 'Jonas');
 console.log('' || 'Jonas');
@@ -66,12 +66,25 @@ console.log(undefined || null);
 
 console.log(undefined || 0 || '' || 'Hello' || 23 || null); //cos hello is the first truthy value
 
-restaurant.numGuests = 23;
+restaurant.numGuests = 23; //it wont work when the numGeust=0
 const guests1 = restaurant.numGuests ? restaurant.numGuests : 10;
 console.log(guests1);
 
 const guests2 = restaurant.numGuests || 10;
 console.log(guests2);
+
+console.log('---- AND ----');
+console.log(0 && 'Jonas');
+console.log(7 && 'Jonas');
+
+console.log('Hello' && 23 && null && 'jonas');
+
+// Practical example
+if (restaurant.orderPizza) {
+  restaurant.orderPizza('mushrooms', 'spinach');
+}
+
+restaurant.orderPizza && restaurant.orderPizza('mushrooms', 'spinach');
 
 /*
 ///////////////////////////////////////
