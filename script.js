d83fd5c0dc699551abd25dae0af64cc83e8f5242
Oyindamola32 <oyindamolatolulope82@gmail.com>
2022-11-12 22:27:09 +0530

    modify script.js

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ const openingHours = {
 
 const restaurant = {
   name: 'Classico Italiano',
-  location: 'Via Angelo Tavanti 23, Firenze, france',
+  location: 'Via Angelo Tavanti 23, Firenze, Italy',
   categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
