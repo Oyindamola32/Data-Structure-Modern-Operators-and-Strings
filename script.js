38ce4e8af9dcba54ad04e812a718e294677daf57
Oyindamola32 <oyindamolatolulope82@gmail.com>
2022-11-20 21:26:13 +0530

    modify scropt.js

--- script.js
+++ script.js
@@ -37,9 +37,7 @@ const restaurant = {
   },
 
   orderPasta(ing1, ing2, ing3) {
-    console.log(
-      `Here is your declicious pasta with ${ing1}, ${ing2} and ${ing3}`
-    );
+    console.log(`Here is your declicious pasta ${ing1}, ${ing2} and ${ing3}`);
   },
 
   orderPizza(mainIngredient, ...otherIngredients) {
