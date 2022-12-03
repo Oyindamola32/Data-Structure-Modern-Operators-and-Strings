01859391689685ab1410ca3bab2ad9c52006b7d4
Oyindamola32 <oyindamolatolulope82@gmail.com>
2022-12-03 22:26:45 +0530

    modify script.js

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 'use strict';
 
-const weekdays = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun', 'fri'];
+const weekdays = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
 const openingHours = {
   [weekdays[3]]: {
     open: 12,
     close: 22,
   },
-  [weekdays[4]]: {
+  [weekends[4]]: {
     open: 11,
     close: 23,
   },
