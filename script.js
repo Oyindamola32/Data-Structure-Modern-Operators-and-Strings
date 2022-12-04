65a35611593cead4529b02ad8862b6d9c0dfcffc
Oyindamola32 <oyindamolatolulope82@gmail.com>
2022-12-04 18:38:16 +0530

    modify script

--- script.js
+++ script.js
@@ -7,8 +7,8 @@ const openingHours = {
     close: 22,
   },
   [weekdays[4]]: {
-    open: 19,
-    close: 32,
+    open: 11,
+    close: 23,
   },
   [weekdays[5]]: {
     open: 0, // Open 24 hours
