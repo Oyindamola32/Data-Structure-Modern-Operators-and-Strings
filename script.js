dff150ed79b33d77cf31a3c3beb5eaf052dd4583
Oyindamola32 <oyindamolatolulope82@gmail.com>
2022-12-05 14:00:27 +0530

    script.js

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ const openingHours = {
   },
 };
 
-const restaurants = {
+const restaurant = {
   name: 'Classico Italiano',
   location: 'Via Angelo Tavanti 23, Firenze, Italy',
   categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
@@ -30,7 +30,7 @@ const restaurants = {
     return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
   },
 
-  orderDeliveryp({ starterIndex = 1, mainIndex = 0, time = '20:00', address }) {
+  orderDelivery({ starterIndex = 1, mainIndex = 0, time = '20:00', address }) {
     console.log(
       `Order received! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`
     );
