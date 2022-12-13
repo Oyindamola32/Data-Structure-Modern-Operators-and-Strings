b9852d622f555b0283f21c450ee49e6603af0610
Oyindamola32 <oyindamolatolulope82@gmail.com>
2022-12-13 13:17:35 +0530

    modify script.js

--- script.js
+++ script.js
@@ -30,7 +30,7 @@ const restaurant = {
     return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
   },
 
-  orderDelivery({ starterIndex = 8, mainIndex = 9, time = '20:00', address }) {
+  orderDelivery({ starterIndex = 1, mainIndex = 0, time = '20:00', address }) {
     console.log(
       `Order received! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`
     );
