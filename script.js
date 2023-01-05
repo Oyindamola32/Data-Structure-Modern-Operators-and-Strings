c862f3b223286566df4ab7f941f39852bad95baf
Oyindamola32 <oyindamolatolulope82@gmail.com>
2023-01-05 11:54:50 +0530

    modify script.js

--- script.js
+++ script.js
@@ -36,13 +36,13 @@ const restaurant = {
     );
   },
 
-  orderPastaf(ing1, ing2, ing3) {
+  orderPasta(ing1, ing2, ing3) {
     console.log(
       `Here is your declicious pasta with ${ing1}, ${ing2} and ${ing3}`
     );
   },
 
-  s(mainIngredient, ...otherIngredients) {
+  orderPizza(mainIngredient, ...otherIngredients) {
     console.log(mainIngredient);
     console.log(otherIngredients);
   },
